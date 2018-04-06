8ed726586802da8bd94563311b4c3e46df14c32e
Lucas Klassmann <lucasklassmann@gmail.com>
2018-04-06 17:56:04 -0300

    Optimizing messages and code layout for PEP8

--- app.py
+++ app.py
@@ -1,53 +1,64 @@
 
-import settings
+import requests
+import random
+import string
+import httplib2
+import json
 
 from flask import Flask
 from flask import render_template, request, redirect, url_for, flash
 from flask import jsonify
 from flask import session
+from flask import make_response
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.client import FlowExchangeError
-import httplib2
-import json
-from flask import make_response
-import requests
-import random
-import string
+
+import settings
+from database import Category, Base, Item
 
 CLIENT_ID = json.loads(
     open('client_secret_google.json', 'r').read())['web']['client_id']
 
+MSG_NOT_AUTHORIZED = 'You do not have authorization for {}.'
+
 app = Flask(__name__)
 app.secret_key = settings.SECRET
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from database import Category, Base, Item
-
 engine = create_engine(settings.DATABASE)
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 dbsession = DBSession()
 
+
 def get_state_token():
-    return ''.join(random.choice(string.ascii_uppercase + string.digits)
-                    for x in range(32))
+    return ''.join(
+        random.choice(string.ascii_uppercase + string.digits)
+        for x in range(32)
+    )
+
+
 def response_success(msg):
     response = make_response(json.dumps(msg), 200)
     response.headers['Content-Type'] = 'application/json'
     return response
+
 
 def response_error(msg):
     response = make_response(json.dumps(msg), 401)
     response.headers['Content-Type'] = 'application/json'
     return response
 
+
 def is_loggedin():
     return 'access_token' in session and 'gplus_id' in session
 
+
 def get_userid():
     return session.get('gplus_id')
+
 
 # API
 @app.route('/api/catalog.json')
@@ -55,10 +66,12 @@ def api_catalog():
     categories = dbsession.query(Category).all()
     return jsonify(Category=[r.serialize for r in categories])
 
+
 @app.route('/api/<int:category_id>/category.json')
 def api_catalog_category(category_id):
     category = dbsession.query(Category).filter_by(id=category_id).one()
     return jsonify(Category=category.serialize)
+
 
 # Front-End
 @app.route('/')
@@ -66,9 +79,12 @@ def index():
     has_add_permission = is_loggedin()
     session['state'] = get_state_token()
     categories = dbsession.query(Category).all()
-    return render_template('index.html', 
-        categories=categories, 
-        has_add_permission=has_add_permission)
+    return render_template(
+        'index.html',
+        categories=categories,
+        has_add_permission=has_add_permission
+    )
+
 
 # Category
 @app.route('/category/<int:category_id>/')
@@ -77,18 +93,23 @@ def category_view(category_id):
     session['state'] = get_state_token()
     category = dbsession.query(Category).filter_by(id=category_id).one()
     items = dbsession.query(Item).filter_by(category_id=category.id).all()
-    return render_template('category/view.html', 
-        category=category, 
+    return render_template(
+        'category/view.html',
+        category=category,
         items=items,
-        has_add_permission=has_add_permission)
+        has_add_permission=has_add_permission
+    )
+
+
 
 @app.route('/category/new/', methods=['GET', 'POST'])
 def category_new():
     if not is_loggedin():
-        flash('You are not logged-in, you do not have authorization for add a new category.')
+        flash(MSG_NOT_AUTHORIZED.format('add a new category'))
         return redirect('/')
     
     session['state'] = get_state_token()
+
     if request.method == 'POST':
         name = request.form['name']
         description = request.form['description']
@@ -99,12 +120,13 @@ def category_new():
         return redirect(url_for('index'))
     return render_template('category/new.html')
 
+
 @app.route('/category/<int:category_id>/edit/', methods=['GET', 'POST'])
 def category_edit(category_id):
     category = dbsession.query(Category).filter_by(id=category_id).one()
 
     if not is_loggedin():
-        flash('You are not logged-in, you do not have authorization for update the category.')
+        flash(MSG_NOT_AUTHORIZED.format('update the category'))
         return redirect('/')
 
     if category.gplus_id != session.get('gplus_id'):
@@ -125,16 +147,17 @@ def category_edit(category_id):
         return redirect(url_for('index'))
     return render_template('category/edit.html', category=category)
 
+
 @app.route('/category/<int:category_id>/delete/', methods=['GET', 'POST'])
 def category_delete(category_id):
     category = dbsession.query(Category).filter_by(id=category_id).one()
     
     if not is_loggedin():
-        flash('You are not logged-in, you do not have authorization for delete the category.')
+        flash(MSG_NOT_AUTHORIZED.format('delete the category'))
         return redirect('/')
 
     if category.gplus_id != session.get('gplus_id'):
-        flash('You do not have authorization for delete this category.')
+        flash(MSG_NOT_AUTHORIZED.format('delete this category'))
         return redirect('/')
 
     session['state'] = get_state_token()
@@ -147,12 +170,13 @@ def category_delete(category_id):
         return redirect(url_for('index'))
     return render_template('category/delete.html', category=category)
 
+
 # Item
 @app.route('/category/<int:category_id>/item/new/', methods=['GET', 'POST'])
 def item_new(category_id):
 
     if not is_loggedin():
-        flash('You are not logged-in, you do not have authorization for add a new item.')
+        flash(MSG_NOT_AUTHORIZED.format('add a new item'))
         return redirect('/')
 
     session['state'] = get_state_token()
@@ -170,17 +194,18 @@ def item_new(category_id):
         return redirect(url_for('category_view', category_id=category.id))
     return render_template('item/new.html', category=category)
 
+
 @app.route('/category/<int:category_id>/item/<int:item_id>/edit/', methods=['GET', 'POST'])
 def item_edit(category_id, item_id):
     category = dbsession.query(Category).filter_by(id=category_id).one()
     item = dbsession.query(Item).filter_by(id=item_id).one()
 
     if not is_loggedin():
-        flash('You are not logged-in, you do not have authorization for update the item.')
+        flash(MSG_NOT_AUTHORIZED.format('update the item'))
         return redirect('/')
 
     if item.gplus_id != session.get('gplus_id'):
-        flash('You do not have authorization for update this item.')
+        flash(MSG_NOT_AUTHORIZED.format('update this item'))
         return redirect('/')
 
     session['state'] = get_state_token()
@@ -197,17 +222,18 @@ def item_edit(category_id, item_id):
         return redirect(url_for('category_view', category_id=category.id))
     return render_template('item/edit.html', category=category, item=item)
 
+
 @app.route('/category/<int:category_id>/item/<int:item_id>/delete/', methods=['GET', 'POST'])
 def item_delete(category_id, item_id):
     category = dbsession.query(Category).filter_by(id=category_id).one()
     item = dbsession.query(Item).filter_by(id=item_id).one()
 
     if not is_loggedin():
-        flash('You are not logged-in, you do not have authorization for delete the item.')
+        flash(MSG_NOT_AUTHORIZED.format('delete the item'))
         return redirect('/')
 
     if item.gplus_id != session.get('gplus_id'):
-        flash('You do not have authorization for delete this item.')
+        flash(MSG_NOT_AUTHORIZED.format('delete this item'))
         return redirect('/')
 
     session['state'] = get_state_token()
@@ -277,6 +303,7 @@ def gconnect():
     session['picture'] = data['picture']
     session['email'] = data['email']
     return response_success('You are logged in.')
+
 
 @app.route('/google/disconnect/')
 def gdisconnect():
