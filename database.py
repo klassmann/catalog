d2aa102f2610638e9beea6645c6bbdf196171221
Lucas Klassmann <lucasklassmann@gmail.com>
2018-04-06 18:04:56 -0300

    database.py pep8

--- database.py
+++ database.py
@@ -1,5 +1,4 @@
 import sys
-
 import settings
 
 from sqlalchemy import Column, ForeignKey, Integer, String, Text
@@ -11,41 +10,44 @@ from flask import url_for
 
 Base = declarative_base()
 
+
 class Category(Base):
     __tablename__ = 'category'
-    id = Column(Integer, primary_key = True)
-    name = Column(String(200), nullable = False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(200), nullable=False)
     description = Column(Text())
     items = relationship("Item", back_populates="category")
-    gplus_id = Column(String(200), nullable = False)
+    gplus_id = Column(String(200), nullable=False)
 
     @property
     def serialize(self):
         return {
-            'name' : self.name,
-            'id' : self.id,
-            'items' : [x.serialize for x in self.items],
+            'name': self.name,
+            'id': self.id,
+            'items': [x.serialize for x in self.items],
             'url': url_for('api_catalog_category', category_id=self.id)
         }
 
+
 class Item(Base):
     __tablename__ = 'item'
-    id = Column(Integer, primary_key = True)
-    name = Column(String(200), nullable = False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(200), nullable=False)
     description = Column(Text())
     category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category, back_populates="items")
-    gplus_id = Column(String(200), nullable = False)
+    gplus_id = Column(String(200), nullable=False)
 
     @property
     def serialize(self):
         return {
-            'name' : self.name,
-            'description' : self.description,
-            'id' : self.id,
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
         }
 
-# If this script is called, 
+
+# If this script is called,
 # it will create de database and table structures
 if __name__ == '__main__':
     engine = create_engine(settings.DATABASE)
